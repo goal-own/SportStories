93048f7b235ea4d9dfa0276e5f69cf3c8047facc
Malik <hiraevmalik@yandex.ru>
2019-10-06 23:10:52 +0300

    Remove unnecessary add stories layout

--- app/src/main/java/org/sportsstories/presentation/base/stories/AddStoriesViewHolder.kt
+++ app/src/main/java/org/sportsstories/presentation/base/stories/AddStoriesViewHolder.kt
@@ -2,5 +2,14 @@ package org.sportsstories.presentation.base.stories
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.view_stories.view.item_stories_text
+import org.sportsstories.R
+import ru.touchin.extensions.getString
 
-class AddStoriesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+class AddStoriesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    init {
+        itemView.item_stories_text.text = getString(R.string.stories_add)
+    }
+
+}
